81f8b2106c5fe764eb84d92fd0cc1d904992cc39
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-01-15 23:04:25 +0300

    build(constants): Mettre a jour les constantes de l'application

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   // Database Configuration
   // Configuration stockée dans database_config.dart
   static const String appName = 'planificator';
-  static const String appVersion = '2.0.0';
+  static const String appVersion = '2.1.1';
   static const String appBuild = '1';
 
   // Messages
