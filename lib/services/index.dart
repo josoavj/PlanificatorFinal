7ac1e4f2740894220dc476231619c5df4982b977
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-03-29 07:38:09 +0300

    chore: exporter ThemeProvider depuis services/index.dart
    - Ajouter l'export du ThemeProvider
    - Nettoyer le formatage du fichier (espacements)

--- lib/services/index.dart
+++ lib/services/index.dart
@@ -1,5 +1,7 @@
 /// Fichier index pour exporter tous les services
 library;
+
 export 'database_service.dart';
 export 'logging_service.dart';
 export 'notification_service.dart';
+export 'theme_provider.dart';
