d09e5729be602d465dab3c4d5fcbf4b2b758bb9b
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2026-07-19 10:13:30 +0300

    Update historique_screen.dart

--- lib/screens/historique/historique_screen.dart
+++ lib/screens/historique/historique_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 import '../../models/index.dart';
 import '../../repositories/index.dart';
 import '../../widgets/index.dart';
